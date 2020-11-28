6966ce1abce451baa0e8e1d40ad45185f2a7c05e
Ard-Jan van Etten <ardjanve@gmail.com>
2020-11-28 16:13:50 +0100

    werkt

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <math.h>
 
 const int SCENEGROOTTE = 2;
+float renderafstand = 10000000.0;
+float minimumafstand = 0.0001;
+
+int breedte = 100;
+int hoogte = 40;
+
 
 struct Vector3 {
 	float x;
@@ -15,7 +23,14 @@ struct Bol {
 	struct Vector3 positie;
 };
 
+struct Camera {
+	float lat;
+	float lon;
+	struct Vector3 positie;
+};
+
 struct Bol *scene[SCENEGROOTTE];
+struct Camera *camera;
 
 float afstand3 (struct Vector3 *a, struct Vector3 *b) {
 	float ongeworteldeAfstand = 
@@ -33,11 +48,69 @@ float bolAfstand (struct Vector3 *positie, struct Bol *bol) {
 }
 
 float dichtsbijzijndeBolAfstand (struct Vector3 *positie) {
-	int kleinsteAfstand = 10000000.0; // come at me 
+	float kleinsteAfstand = renderafstand;
 
 	for (int i = 0; i < SCENEGROOTTE; i++) {
-		
+		float afstand = bolAfstand(positie, scene[i]);
+		kleinsteAfstand = (afstand < kleinsteAfstand) ? afstand : kleinsteAfstand;	
 	}
+
+	return kleinsteAfstand;
+}
+
+bool raakt (int x, int y) {
+	float lat = (*camera).lat + ((float) y / (float) hoogte - 0.5) * M_PI;
+	float lon = (*camera).lon + ((float) x / (float) breedte - 0.5) * M_PI;
+
+	struct Vector3 koers = {
+		cos(lon) * sin(lat),
+		sin(lon) * sin(lat),
+		cos(lat)
+	};
+
+	float totaleafstand = 0;
+	struct Vector3 straalPositie = (*camera).positie;
+	float afstand = dichtsbijzijndeBolAfstand(&straalPositie);
+	// printf("positie: %.4f, %.4f, %.4f\n",straalPositie.x, straalPositie.y, straalPositie.z );
+
+	while (true) {
+		straalPositie.x += koers.x * afstand;
+		straalPositie.y += koers.y * afstand;
+		straalPositie.z += koers.z * afstand;
+
+		totaleafstand += afstand;
+		afstand = dichtsbijzijndeBolAfstand(&straalPositie);
+
+		// printf("%.6f, %.6f \n", afstand, minimumafstand);
+
+		if (afstand < minimumafstand) {
+			return true;
+		}
+
+		if (totaleafstand >= renderafstand) {
+			return false;
+		}
+	}
+}
+
+void tekenframe () {
+	for (int y = 0; y < hoogte; y++) {
+		for (int x = 0; x < breedte; x++) {
+			if (raakt(x, y)) {
+				printf("X");
+			} else {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+
+	(*camera).positie.x += 0.125;
+	(*camera).positie.y += 0.8;
+	(*camera).positie.z += 0.0;
+
+	(*camera).lat += 0.0;
+	(*camera).lon += 0.01745;
 }
 
 int main () {
@@ -48,22 +121,35 @@ int main () {
 	};
 
 	struct Bol bol1 = {
-		5.0,
+		3.0,
 		{
 			10.0,
-			10.0,
-			10.0
+			0.0,
+			0.0
 		}
 	};
 
 	struct Bol bol2 = {
-		3.0,
+		5.0,
 		{
-			4.0,
-			4.0,
-			4.0
+			5.0,
+			10.0,
+			0.0
 		}
 	};
+
+	struct Camera camera1 = {
+		1.570796,
+		0.0,
+		{
+			0.0,
+			0.0,
+			0.0
+		}
+	};
+
+	camera = malloc(sizeof(struct Camera));
+	*camera = camera1;
 
 	for (int i = 0; i < SCENEGROOTTE; i++) {
 		scene[i] = malloc(sizeof(struct Bol));
@@ -72,8 +158,12 @@ int main () {
 	*scene[0] = bol1;
 	*scene[1] = bol2;
 
-	float afstand = bolAfstand(&centrum, &bol1);
-	printf("%.6f\n", afstand);	
+	for (int frame = 0; frame < 50; frame++) {
+		printf("\e[1;1H\e[2J");	
+		usleep(100000);
+
+		tekenframe();	
+	}
 }
 
 
